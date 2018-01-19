565412d2adb3c06069101bc9fa046591e987ba93
Antti Koivumäki <anttko@ety-a2123b-31.edu.wks.metropolia.fi>
2018-01-19 11:46:30 +0200

    teht b

--- src/app/http-test/http-test.component.ts
+++ src/app/http-test/http-test.component.ts
@@ -9,21 +9,48 @@ import {HttpClient} from '@angular/common/http';
 export class HttpTestComponent implements OnInit {
 
   tulos = 'Moro';
+  apitulos = 'apitulos';
+  apiosoite = 'http://media.mw.metropolia.fi/wbma';
+  kuvaosoite = 'http://media.mw.metropolia.fi/wbma/uploads/';
+  autoosote = 'https://api.openchargemap.io/v2/poi/';
+  tulokset = 'ag';
+  autotulokset = '';
 
   constructor(private http: HttpClient) { }
 
-  getJson() {
-    interface Myinterface {
-      license: string;
+    getJson() {
+        interface Myinterface {
+            license: string;
+        }
+
+        this.http.get<Myinterface>('assets/package.json').subscribe(data => {
+            console.log(data);
+            this.tulos = data.license;
+        });
     }
-    this.http.get<Myinterface>('assets/package.json').subscribe( data => {
-      console.log(data);
-      this.tulos = data.license;
-    });
-  }
+
+    getFromApi() {
+        this.http.get(this.apiosoite + '/media').subscribe(data => {
+            console.log(data[0].filename);
+            this.tulokset = data;
+        });
+    }
+
+
+  getCarStations () {
+        this.http.get(this.autoosote).subscribe(data => {
+            console.log(data.ID);
+            this.autotulokset = this.autoosote + data[0];
+        });
+
+    }
+
+
 
   ngOnInit() {
     this.getJson();
+    this.getFromApi();
+    this.getCarStations();
   }
 
 }
